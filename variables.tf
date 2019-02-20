02dda2d1daf4ce8950604cf63790fc2af0ec4ad3
priyankafnu05 <47825013+priyankafnu05@users.noreply.github.com>
2019-02-20 10:53:20 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+#File holds the variables to reference in the Main.tf
+
+variable "fromserver_port" {
+	description = "The port the server will use for HTTP requests"
+	default = 20
+}
+variable "toserver_port" {
+        description = "The port the server will use for HTTP requests"
+        default = 500
+}
+variable "healthchk_port" {
+        description = "The port the server will use for HTTP requests"
+        default = 443
+}
